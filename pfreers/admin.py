1a4a8c87980164a5bf36caa4b7a35a5fc09e31c1
Patrick Lim <guruwiz7@gmail.com>
2021-08-10 22:53:35 +0900

    delete pfreers

--- pfreers/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import PfreeEateries
-
-# Register your models here.
-admin.site.register(PfreeEateries)
--- pfreers/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PfreersConfig(AppConfig):
-    name = 'pfreers'
